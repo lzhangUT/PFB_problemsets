f9f386bb94667e00e06bd7a073e4ca51311ff31e
Li Zhang <zhanglicau@gmail.com>
2019-09-07 22:43:08 -0500

    :quit

---
+++ test.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+import sys
+myname = sys.argv[1]
+mycolor =sys.argv[2]
+
+print('My name is:', myname,  'My favorite color:',mycolor)
+print('My name is:' +     myname +',' + 'My favorite color:'+mycolor)
